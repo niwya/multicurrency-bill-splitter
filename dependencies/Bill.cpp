0e7e1cb03041fdd6891a2b74dabbd3b364d1e5b9
Chloe Benz <chloe.benz@outlook.com>
2022-09-01 16:08:32 -0500

    More Item/Bill

--- dependencies/Bill.cpp
+++ dependencies/Bill.cpp
@@ -1,1 +1,25 @@
 #include "Bill.h"
+
+// Setters
+void Bill::setTaxPercentage(double taxPercentage) {
+    _tax = this->getTotal()*taxPercentage;
+}
+void Bill::setTipPercentage(double tipPercentage) {
+    _tip = this->getTotal()*tipPercentage;
+}
+
+// Other
+void Bill::computeTotal() {
+    // Reset the total to 0 to avoid any issue
+    this->resetTotal();
+    // And add the price to each item
+    for (auto it = _itemList.begin(); it != _itemList.end(); it++) {
+        _billTotal += it->getPrice();
+    }
+} 
+void Bill::addItem(Item newItem) {
+    this->_itemList.push_back(newItem);
+}
+std::ostream& operator<<(std::ostream& ostream, const Bill& bill) {
+    // do
+}
--- dependencies/Bill.h
+++ dependencies/Bill.h
@@ -7,11 +7,11 @@
 #include <vector>
 
 class Item {
-    public:
+    private:
         std::string _itemName;
         double _itemPrice;
         std::vector<User> _itemUsers;
-    private:
+    public:
         // Constructor
         Item() {};
         Item(std::string name) {_itemName = name;};
@@ -19,22 +19,37 @@ class Item {
         Item(std::string name, double price, std::vector<User> users) {_itemName = name; _itemPrice = price; _itemUsers = users;};
         // Destructor
         // Getters
+        double getPrice() const {return _itemPrice;};
         // Setters
 
 };
 
 class Bill {
-    public:
+    private:
         std::vector<Item> _itemList;
         double _billTotal;
+        double _tax;
+        double _tip;
         ReferenceCurrency _billCurrency;
-    private:
+    public:
         // Constructor
-        Bill(ReferenceCurrency billCurrency) {_billCurrency = billCurrency;};
-        Bill(ReferenceCurrency billCurrency, std::vector<Item> itemList) {_billCurrency = billCurrency; _itemList = itemList;};
+        Bill(ReferenceCurrency billCurrency) {_billCurrency = billCurrency; _billTotal = 0.0; _tax = 0.0; _tip = 0.0;};
+        Bill(ReferenceCurrency billCurrency, std::vector<Item> itemList) {_billCurrency = billCurrency; _itemList = itemList; _billTotal = 0.0; _tax = 0.0; _tip = 0.0;};
         // Destructor
         // Getters
+        double getTotal() const {return _billTotal;};
+        ReferenceCurrency getCurrency() const {return _billCurrency;};
+        std::vector<Item> getItems() const {return _itemList;};
         // Setters
+        void resetTotal() {_billTotal = 0.0;};
+        void setTaxAmount(double tax) {_tax = tax;}
+        void setTipAmount(double tip) {_tip = tip;};
+        void setTaxPercentage(double taxPercentage); // add catch error when perc not b/w 0 and 1
+        void setTipPercentage(double tipPercentage); // add catch error when perc not b/w 0 and 1
+        // Other
+        void computeTotal();
+        void addItem(Item newItem);
+        friend std::ostream& operator<<(std::ostream& ostream, const Bill& bill); // NYE
 
 };
 
